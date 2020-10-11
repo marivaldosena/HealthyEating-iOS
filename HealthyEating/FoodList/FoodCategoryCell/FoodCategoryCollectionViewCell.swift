6d58babc543d7b1cf18bb86276cbff9b1247ad32
Marivaldo Sena <marivaldo.sena@live.com>
2020-10-11 17:46:36 -0300

    Create food category and food items sketch.

--- HealthyEating/FoodList/FoodCategoryCell/FoodCategoryCollectionViewCell.swift
+++ HealthyEating/FoodList/FoodCategoryCell/FoodCategoryCollectionViewCell.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
+@IBDesignable
 class FoodCategoryCollectionViewCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    @IBInspectable
+    var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
     }
 
 }
